5a1b513f80bc9071268482f6d5352a025f6a0224
Georgi Iliev <neokaozzz@gmail.com>
2015-04-21 21:48:13 +0300

    Altered table structure

--- War/src/sql/schema.sql
+++ War/src/sql/schema.sql
@@ -4,9 +4,9 @@ CREATE DATABASE `warehouse`;
 
 CREATE TABLE `warehouse`.`products` (
 	`id` TINYINT NOT NULL  AUTO_INCREMENT ,
-	`group` VARCHAR( 255 ) NOT NULL ,
-	`brand` VARCHAR( 255 ) NOT NULL ,
-	`model` VARCHAR( 255 ) NOT NULL ,
+	`group` VARCHAR( 100 ) NOT NULL ,
+	`brand` VARCHAR( 30 ) NOT NULL ,
+	`model` VARCHAR( 10 ) NOT NULL ,
 	`quantity` SMALLINT NOT NULL ,
 	`price` FLOAT NOT NULL ,
 	PRIMARY KEY ( `id` )
@@ -15,12 +15,12 @@ CREATE TABLE `warehouse`.`products` (
 CREATE TABLE `warehouse`.`customers` (
 	`id` TINYINT NOT NULL  AUTO_INCREMENT ,
 	`name` VARCHAR( 255 ) NOT NULL ,
-	PRIMARY KEY ( `id` )
+	PRIMARY KEY ( `id` ),
 ) ENGINE = InnoDB;
 
 CREATE TABLE `warehouse`.`discounts` (
 	`id` TINYINT NOT NULL  AUTO_INCREMENT ,
-	`name` VARCHAR( 255 ) NOT NULL ,
+	`name` VARCHAR( 50 ) NOT NULL ,
 	`required_quantity`  SMALLINT NULL DEFAULT 0 ,
 	PRIMARY KEY ( `id` )
 ) ENGINE = InnoDB;
@@ -31,7 +31,7 @@ CREATE TABLE `warehouse`.`orders` (
 	`order_quantity` SMALLINT NOT NULL ,
 	`customer_id` TINYINT NOT NULL,
 	`discount_id` TINYINT NULL DEFAULT 0,
-	
+	`reduced_price` FLOAT NOT NULL ,
 	PRIMARY KEY ( `id` ) ,
 	FOREIGN KEY ( `customer_id` )
 		REFERENCES `warehouse`.`customers`( `id` )
