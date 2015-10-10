2b33f928d3eb1f4ed038b87d68390a066a78f59a
Georgi Iliev <neokaozzz@gmail.com>
2015-10-11 02:05:52 +0300

    Complete reformat

--- War/src/warehouse/Warehouse_Server.java
+++ War/src/warehouse/Warehouse_Server.java
@@ -7,42 +7,40 @@ import java.sql.DriverManager;
 
 public class Warehouse_Server {
 
-	public static final int port=4444;
-	public static String url="jdbc:mysql://localhost:3306/warehouse";     
-	public static String password="BASKET14fena";							
-	public static String user="root";
+    public static final int port = 4444;
+    public static String url = "jdbc:mysql://localhost:3306/warehouse";
+    public static String password = "BASKET14fena";
+    public static String user = "root";
 
-		
-	public static void main(String[] args)throws Exception {
-		//Makes the connection to DB
-		Class.forName("com.mysql.jdbc.Driver");
-		Connection con=DriverManager.getConnection( url, user, password);
-		//
-		
-		ServerSocket svsock=null;
-		Socket connection;
-		Thread customer;
-		
-	try{
-			svsock=new ServerSocket(port);
-			
-			System.out.println("Server is successfully started.");
-			
-			while(true){
-			System.out.println("Waiting for clients to connect...");
-			
-			connection=svsock.accept();
-			
-			customer=new Thread(new Client_Thread(connection,con));
-			customer.start();
-			Thread.yield();
-			
-			System.out.println("Client connected");
-			}
-		}
-			catch(Exception e){
-				System.out.println("Can't start server. "+e.getMessage());
-				}
+    public static void main(String[] args) throws Exception {
+	// Makes the connection to DB
+	Class.forName("com.mysql.jdbc.Driver");
+	Connection con = DriverManager.getConnection(url, user, password);
+	//
 
+	ServerSocket svsock = null;
+	Socket connection;
+	Thread customer;
+
+	try {
+	    svsock = new ServerSocket(port);
+
+	    System.out.println("Server is successfully started.");
+
+	    while (true) {
+		System.out.println("Waiting for clients to connect...");
+
+		connection = svsock.accept();
+
+		customer = new Thread(new Client_Thread(connection, con));
+		customer.start();
+		Thread.yield();
+
+		System.out.println("Client connected");
+	    }
+	} catch (Exception e) {
+	    System.out.println("Can't start server. " + e.getMessage());
 	}
+
+    }
 }
