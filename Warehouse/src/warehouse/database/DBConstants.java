37e2ff49ae483ea4bfdbbd1cc839ae2189e81f5a
Georgi Iliev <neokaozzz@gmail.com>
2016-01-31 15:30:40 +0200

    Samples

--- Warehouse/src/warehouse/database/DBConstants.java
+++ Warehouse/src/warehouse/database/DBConstants.java
@@ -3,7 +3,7 @@ package warehouse.database;
 public class DBConstants {
     
     public static final String URL = "jdbc:mysql://localhost:3306/warehouse";
-    public static final String PASSWORD = "BASKET14fena";
-    public static final String USER = "root";
+    public static final String PASSWORD = "password";
+    public static final String USER = "user";
 
 }
